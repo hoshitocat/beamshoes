d26224f9cadf932355db90d95ffb85e1a0bed8a8
junishitsuka <ishitsuka.jun@gmail.com>
2015-10-25 11:47:39 +0900

    Sidekiqの管理画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   resources :sound_logs, only: %i(create)
+  mount Sidekiq::Web, at: "/sidekiq"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
